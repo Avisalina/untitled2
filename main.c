262ae487a176b9a3d183cfde09add469a7ff9c84
Klaudia <245759@edu.p.lodz.pl>
2022-10-09 08:56:21 +0200

    Zmiany dodatkowe w 03

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int a=2, b=3, c=a*b;
+    int a=2, b=3, c=a*b, d=c*b+a;
 
     printf("Hello, World!\n");
-    printf("W pierwszym branchu; c= %d",c);
+    printf("W pierwszym branchu; d=%d", d);
     return 0;
 }
